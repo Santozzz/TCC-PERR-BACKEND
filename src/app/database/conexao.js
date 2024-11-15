27420d86d36dacbe30211d70fe43eaaacc350c91
santozzz <davd.c9ol@gmail.com>
2024-11-15 16:29:33 -0300

    gh

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,35 +1,32 @@
-// Importando o pacote pg de forma compatível com ES6
-import pkg from 'pg';
-const { Client } = pkg;
+import mysql from 'mysql'
 
-// Configurando a conexão com o banco de dados PostgreSQL
-const client = new Client({
-  host: process.env.DB_HOST,      // Exemplo: db.supabase.co
-  port: process.env.DB_PORT || 5432, // Porta padrão para PostgreSQL
-  user: process.env.DB_USER,      // Usuário do banco de dados
-  password: process.env.DB_PASSWORD, // Senha do banco de dados
-  database: process.env.DB_NAME,  // Nome do banco de dados
-});
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_perr',
+    port: 3306
+})
 
-// Conectando ao banco de dados
-client.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err.stack);
-  } else {
-    console.log('Conectado ao banco de dados PostgreSQL na Supabase');
-  }
-});
+conexao.connect()
 
-// Função para executar consultas SQL
-export const consult = (sql, values = []) => {
-  return new Promise((resolve, reject) => {
-    client.query(sql, values, (err, result) => {
-      if (err) {
-        return reject(new Error(`Erro na consulta: ${err.message}`));
-      }
-      return resolve(result.rows);
+/**
+ * Executa um codigo sql com ou sem valores
+ * @param {string} sql instrução sql a ser executada
+ * @param {string=id / [usuario, email, id, vaga, idvagas]} values valores a serem passados  para o sql
+ * @param {string} mesageReject mensagem a ser exibida
+ * @returns objeto da promise
+ */
+export const consult = (sql, values = '', mesageReject = 'Erro na consulta') => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, values, (err, result) => {
+            if (err) {
+                return reject(new Error(`${mesageReject}: ${err.message}`));
+            }
+            const row = JSON.parse(JSON.stringify(result));
+            return resolve(row);
+        });
     });
-  });
 };
 
-export default client;
+export default conexao
