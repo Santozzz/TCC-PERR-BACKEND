5d4350019b902d4328529e289e99f672f9fb61e8
DaniloKeithi <danilomakiya@gmail.com>
2024-11-16 14:14:53 -0300

    dsadasda

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,4 +1,7 @@
-import mysql from 'mysql'
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Carrega as variáveis do arquivo .env
 
 const conexao = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -6,33 +9,23 @@ const conexao = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306
-})
+});
 
 conexao.connect(error => {
     if (error) {
-      console.error('Erro ao conectar ao banco de dados:', error);
-      return;
+        console.error('Erro ao conectar ao banco de dados:', error);
+        return;
     }
-    console.log('Conectado ao banco de dados MySQL na Railway.');
-  });
-
-/**
- 
-Executa um codigo sql com ou sem valores
-@param {string} sql instrução sql a ser executada
-@param {string=id / [usuario, email, id, vaga, idvagas]} values valores a serem passados  para o sql
-@param {string} mesageReject mensagem a ser exibida
-@returns objeto da promise*/
+    console.log('Conectado ao banco de dados MySQL.');
+});
 export const consult = (sql, values = '', mesageReject = 'Erro na consulta') => {
     return new Promise((resolve, reject) => {
         conexao.query(sql, values, (err, result) => {
             if (err) {
-                return reject(new Error($mesageReject, $err.message));
+                return reject(new Error(`${mesageReject}: ${err.message}`)); // Corrigido
             }
             const row = JSON.parse(JSON.stringify(result));
             return resolve(row);
         });
     });
 };
-
-export default conexao
