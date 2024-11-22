a4bde8cdf52233236b05be4ebb3d943652de169c
DaniloKeithi <danilomakiya@gmail.com>
2024-11-21 21:00:19 -0300

    dsadadadada

--- src/app.js
+++ src/app.js
@@ -6,31 +6,14 @@ import routes from './routes.js';
 
 const app = express();
 
-// Middleware CORS
-app.use(cors({
-  origin: '*', // Domínio do frontend hospedado
-  credentials: true, // Permitir envio de cookies/sessões
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-}));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://tcc-perr-frontend.vercel.app'); // Permitir o frontend
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true'); // Permitir cookies/sessões
-  next();
-});
-
-
-// Middleware Body Parser
+// Middlewares
+app.use(cors({ credentials: true }));
 app.use(bodyParser.json());
-
-// Middleware de Sessão
 app.use(session({
-  secret: 'seu_segredo_aqui', // Substitua por uma string segura
+  secret: 'seu_segredo_aqui',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }, // Altere para 'true' em produção (HTTPS)
+  cookie: { secure: false } // Altere para 'true' se usar HTTPS
 }));
 
 // Rotas
