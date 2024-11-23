9c658d771a34e09997ad9b072c3ab711a49862bb
DaniloKeithi <danilomakiya@gmail.com>
2024-11-23 19:21:22 -0300

    dasdadadadsa

--- src/app.js
+++ src/app.js
@@ -3,34 +3,17 @@ import session from 'express-session';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes.js';
-import connectPgSimple from 'connect-pg-simple';
 
 const app = express();
 
 // Middlewares
 app.use(cors({ credentials: true }));
-
 app.use(bodyParser.json());
-const PgStore = connectPgSimple(session);
-
 app.use(session({
-    store: new PgStore({
-        conObject: { // Configuração da conexão ao banco de dados
-            host: 'aws-0-sa-east-1.pooler.supabase.com',
-            user: 'postgres.inhjqkfiufeoxiqqxypm',
-            password: 'TccPerr2024@',
-            database: 'postgres',
-            port: 6543,
-        }
-    }),
-    secret: 'chave-secreta', // Substitua por uma chave segura
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false, // Use 'true' apenas em produção com HTTPS
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 // 1 dia
-    }
+  secret: 'seu_segredo_aqui',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Altere para 'true' se usar HTTPS
 }));
 
 // Rotas
